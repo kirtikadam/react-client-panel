f26ff545c4193d062778e46ee451a0071cb1fee8
Kirti Kadam <kkadam@pdxinc.com>
2021-05-05 19:11:38 +0530

    notify alert component completed with passing data to entire application and other settings

---
+++ src/reducers/notifyReducer.js
@@ -0,0 +1,20 @@
+import { NOTIFY_USER } from "../actions/types";
+
+const initialState = {
+  message: null,
+  messageType: null,
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case NOTIFY_USER:
+      return {
+        ...state,
+        message: action.message,
+        messageType: action.messageType,
+      };
+    default:
+      return state;
+  }
+}
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer } from "redux-firestore";
+import notifyReducer from "./notifyReducer";
+import settingsReducer from "./settingsReducer";
 
 // Add firebase to reducers
 export default combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
+  notify: notifyReducer,
+  settings: settingsReducer,
 });
